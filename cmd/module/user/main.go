078279fc11f2532c9e1c432ff97bc749e98dce66
Douglas <mendes-barreto@Live.com>
2023-08-24 23:43:19 -0400

    feat: Add gRPC server reflection

--- cmd/module/user/main.go
+++ cmd/module/user/main.go
@@ -1,16 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"net"
+	"time"
+
+	"go.uber.org/automaxprocs/maxprocs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log/slog"
 
 	"github.com/mendesbarreto/go-my-coffe-shop/cmd/module/user/config"
+	"github.com/mendesbarreto/go-my-coffe-shop/proto/gen"
 )
 
+type UserGRPCServer struct {
+	gen.UnimplementedUserServiceServer
+}
+
+func NewUserGRPCServer(server *grpc.Server) gen.UserServiceServer {
+	return nil
+}
+
 func main() {
+	//
+	// This function is used to keep the number of operating system threads that can execute user-level Go code at any one time.
+	// When Go starts it's automatically set the number of thread to max available on the system. That means the number of goroutines that can actually run in parallel.
+	// In Kubernetes, all the available CPU cores on the node are visible by its pods If you set a pod CPU limit to 1 core
+	// but your node has 64 cores of CPU, your Go app will grab the actual node resource and set GOMAXPROC to 64
+	// So the maxprocs Set() helps to assign only the number of cores available for the pod.
+	_, err := maxprocs.Set()
+	if err != nil {
+		slog.Error("Problem to set the threads available on the system", err)
+	}
+
+	slog.Info("User Module Starting up")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	slog.Info("User Module context created")
 	config := config.GetConfig()
-	fmt.Printf("App name: %v\n", config)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+
+	slog.Info("User Module Config loaded", config)
+
+	grpcServer := grpc.NewServer()
+	serverAddress := fmt.Sprintf("%s:%s", config.Grcp.Host, config.Grcp.Port)
+	network := "tcp"
+
+	userService := &UserGRPCServer{}
+	gen.RegisterUserServiceServer(grpcServer, userService)
+
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen(network, serverAddress)
+	if err != nil {
+		slog.Info("Failed to start listen to address", err, "network", network, "serverAddress", serverAddress)
+		cancel()
+		<-ctx.Done()
+	}
+
+	slog.Info("User Module Server", "listen", serverAddress)
+
+	defer func() {
+		if err := lis.Close(); err != nil {
+			slog.Error("Problem to close server", err, "network", network, "address", serverAddress)
+		}
+		cancel()
+		<-ctx.Done()
+	}()
+
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		slog.Error("Problem to start gRPC server", err, "network", network, "address", serverAddress)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module github.com/mendesbarreto/go-my-coffe-shop
 
 go 1.20
 
-require github.com/caarlos0/env/v9 v9.0.0
+require (
+	github.com/caarlos0/env/v9 v9.0.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.2
+	google.golang.org/genproto/googleapis/api v0.0.0-20230821184602-ccc8af3d0e93
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	go.uber.org/automaxprocs v1.5.3 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto v0.0.0-20230821184602-ccc8af3d0e93 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230821184602-ccc8af3d0e93 // indirect
+)
+
+replace google.golang.org/grpc => github.com/grpc/grpc-go v1.57.0
