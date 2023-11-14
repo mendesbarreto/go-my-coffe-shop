10c9ab4138d9dbe3414d0aa66b966825b1ed9832
Douglas Mendes <mendes-barreto@live.com>
2023-11-13 22:52:42 -0500

    chore Add get me on user service

--- internal/user/handler/user.go
+++ internal/user/handler/user.go
@@ -6,6 +6,8 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/mendesbarreto/go-my-coffe-shop/internal/user/repository"
+	"github.com/mendesbarreto/go-my-coffe-shop/pkg/model"
 	"github.com/mendesbarreto/go-my-coffe-shop/proto/gen"
 )
 
@@ -14,5 +16,17 @@ func (u *UserGRPCHandler) GetMe(ctx context.Context, req *gen.EmptyRequest) (*ge
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-	return nil, status.Error(codes.NotFound, "This method was not implemented yet")
+
+	userCached := ctx.Value("user").(model.User)
+
+	user, err := repository.GetUserById(ctx, userCached.ID)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &gen.GetUserDetailsResponse{
+		UserId: user.ID.String(),
+		Name:   user.Name,
+		Email:  user.Email,
+	}, nil
 }
