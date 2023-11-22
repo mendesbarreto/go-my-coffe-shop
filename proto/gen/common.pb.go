5274f260108b9eb94e9f38348c9335fef7ad4b20
Douglas Mendes <mendes-barreto@live.com>
2023-11-21 23:10:11 -0500

    chore: Update proto schema

--- proto/gen/common.pb.go
+++ proto/gen/common.pb.go
@@ -12,7 +12,6 @@ import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
-	sync "sync"
 )
 
 const (
@@ -21,44 +20,6 @@ const (
 	// Verify that runtime/protoimpl is sufficiently up-to-date.
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
-
-type EmptyRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-}
-
-func (x *EmptyRequest) Reset() {
-	*x = EmptyRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_common_proto_msgTypes[0]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *EmptyRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*EmptyRequest) ProtoMessage() {}
-
-func (x *EmptyRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_common_proto_msgTypes[0]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use EmptyRequest.ProtoReflect.Descriptor instead.
-func (*EmptyRequest) Descriptor() ([]byte, []int) {
-	return file_common_proto_rawDescGZIP(), []int{0}
-}
 
 var File_common_proto protoreflect.FileDescriptor
 
@@ -70,8 +31,7 @@ var file_common_proto_rawDesc = []byte{
 	0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x2d,
 	0x67, 0x65, 0x6e, 0x2d, 0x6f, 0x70, 0x65, 0x6e, 0x61, 0x70, 0x69, 0x76, 0x32, 0x2f, 0x6f, 0x70,
 	0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x61, 0x6e, 0x6e, 0x6f, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e,
-	0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x0e, 0x0a, 0x0c, 0x45, 0x6d, 0x70, 0x74, 0x79,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x42, 0xd1, 0x02, 0x92, 0x41, 0x65, 0x12, 0x18, 0x0a,
+	0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x42, 0xd1, 0x02, 0x92, 0x41, 0x65, 0x12, 0x18, 0x0a,
 	0x11, 0x4d, 0x79, 0x20, 0x43, 0x6f, 0x66, 0x66, 0x65, 0x20, 0x53, 0x68, 0x6f, 0x70, 0x20, 0x41,
 	0x50, 0x49, 0x32, 0x03, 0x30, 0x2e, 0x31, 0x2a, 0x01, 0x02, 0x72, 0x46, 0x0a, 0x11, 0x4d, 0x79,
 	0x20, 0x43, 0x6f, 0x66, 0x66, 0x65, 0x20, 0x53, 0x68, 0x6f, 0x70, 0x20, 0x41, 0x50, 0x49, 0x12,
@@ -96,22 +56,7 @@ var file_common_proto_rawDesc = []byte{
 	0x74, 0x6f, 0x33,
 }
 
-var (
-	file_common_proto_rawDescOnce sync.Once
-	file_common_proto_rawDescData = file_common_proto_rawDesc
-)
-
-func file_common_proto_rawDescGZIP() []byte {
-	file_common_proto_rawDescOnce.Do(func() {
-		file_common_proto_rawDescData = protoimpl.X.CompressGZIP(file_common_proto_rawDescData)
-	})
-	return file_common_proto_rawDescData
-}
-
-var file_common_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_common_proto_goTypes = []interface{}{
-	(*EmptyRequest)(nil), // 0: api.v1.mycoffeshop.common.EmptyRequest
-}
+var file_common_proto_goTypes = []interface{}{}
 var file_common_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
@@ -125,33 +70,18 @@ func file_common_proto_init() {
 	if File_common_proto != nil {
 		return
 	}
-	if !protoimpl.UnsafeEnabled {
-		file_common_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*EmptyRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_common_proto_goTypes,
 		DependencyIndexes: file_common_proto_depIdxs,
-		MessageInfos:      file_common_proto_msgTypes,
 	}.Build()
 	File_common_proto = out.File
 	file_common_proto_rawDesc = nil
